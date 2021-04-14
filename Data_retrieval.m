88ca7681d0ac061b7d7e0cbddb9b60cd672a1ab0
woodranger50 <woodranger50@gmail.com>
2021-04-14 09:52:19 +0200

    Updated some stuff

--- Data_retrieval.m
+++ Data_retrieval.m
@@ -1,5 +1,5 @@
 %% Fetching experimental data
-clear, clc;
+clf, clear, clc;
 
 
 addpath('C:\Chalmers\Year 3\Mathematical modeling\Project');
@@ -47,34 +47,37 @@ g=9.81;                     %m/s^2
 %Will be changed to for loop for all temp and beakers
 T_water=small_data_1.data(1,2)+273.15;      %K
 m_water=small_data_1.data(1,3)*10^-3;       %kg
-rho_water=rho_water(T_water);               %kg/m^3
-V_water=m_water/rho_water;                  %m^3
-height_water=V_water/(pi*(((beaker_small(4)*(10^(-3)))/2)^2));
-A_water=height_water*beaker_small(3)*pi
+rho_water_var=rho_water(T_water);               %kg/m^3
+V_water=m_water/rho_water_var;                  %m^3
+height_small_water=V_water/(pi*(((beaker_small(4)*(10^(-3)))/2)^2));
+A_small_water=height_small_water*beaker_small(3)*pi;
 %% Convection
 %Vertical wall (free convection vertical cylindar
 %All properties taken at film temperature which is (T_s-T_infinity)/2
-T=273.15;   %Temperature for call functions may be at film temperature
+T_film=(T_surr+T_water)/2;   %Temperature for call functions may be at film temperature
 %Call to functions fetching variables dependent on temperature
-p_water=p_water(T); Pr_water=pr_water(T);   dHvap_water=dHvap_water(T);   
-cp_water=cp_water(T); my_water=my_water(T); k_water=k_water(T); rho_water=rho_water(T);
-cp_air=cp_air(T);   my_air=my_air(T);   k_air=k_air(T); rho_air=rho_air(T);
+p_water_var=p_water(T_film);        Pr_water_var=pr_water(T_film);      dHvap_water_var=dHvap_water(T_film);   
+cp_water_var=cp_water(T_film);      my_water_var=my_water(T_film);      k_water_var=k_water(T_film);        
+rho_water_var=rho_water(T_film);    cp_air_Var=cp_air(T_film);      
+my_air_var=my_air(T_film);          k_air_var=k_air(T_film); rho_air_var=rho_air(T_film);
 
-val=2.035*10^-9;    %taken at 298 K. Needs a function to interpolate   
+val=2.035*10^-9;    %taken at 298 K. Needs a function to interpolate (g*beta*(rho^2)/(my^2) with unit 1/K*m^3)   
 
 %Beaker characteristic heights in mm
-L_large=beaker_large(4);    
-L_small=height_water+(beaker_small(3)-beaker_small(4))*10^-3;   %Change since we need to include convection through glass
+L_large=beaker_large(4);            %Get working for large later
+L_small=height_small_water;
 
 %D/L>=35/(Gr_L^(1/4));       %where d is the cylinder diameter, L is the boundry layer thickness
 C=val;  %C is a value derived from appendix 1 equal to g*beta*(rho^2)/(my^2) with unit 1/K*m^3
 %Lots of good values can be found as a function of temperature in appendix 1
-Gr_L=C*(L_small^3)*dT;   %where b is fluid coefficient of thermal expansion, g is grav. const., L is the significant length, dT is temp. diff., mu is fluid viscosity
-Ra_L=Gr_L*Pr_water;
-Nu_L=((0.825+0.387*(Ra_L^(1/6)))/((1+(0.492/Pr_water)^(9/16))^(8/27)))^2;
-h=Nu_L*k_water/L_small;
-Q=h*A*(T-T_w);
+Gr_L=C*(L_small^3)*(T_film-T_surr);   %where b is fluid coefficient of thermal expansion, g is grav. const., L is the significant length, dT is temp. diff., mu is fluid viscosity
+Ra_L=Gr_L*Pr_water_var;
+Nu_L=((0.825+0.387*(Ra_L^(1/6)))/((1+(0.492/Pr_water_var)^(9/16))^(8/27)))^2;
+h=Nu_L*k_water_var/L_small;
+Q_small=h*A_small_water*(T_film-T_surr)   %KJ
+
 %Horizontal plates (with sides?)
+%not sure if one can use horizontal plates
 
 %% Radiation
 %From sides (cylindrical walls)
@@ -83,7 +86,23 @@ Q=h*A*(T-T_w);
 
 %% Evaporation
 %Water surface
-
+m_flow_matrix=zeros(1,60);
+i=0
+for i=1:60
+    m_flow_1=(small_data_1.data(i+1,3)-small_data_1.data(i,3))/(small_data_1.data(i+1,1)-small_data_1.data(i,1));
+    m_flow_matrix(i)=m_flow_1;
+    temp_matrix(i)=small_data_1.data(i,1);      %finish creating matrix and plot T vs m_flow
+% m_1=small_data_1.data((1:61),3)
+% t_1=small_data_1.data((1:61),2)
+% 
+% m_2=small_data_2.data((1:61),3)
+% t_2=small_data_2.data((1:61),2)
+end
+% figure
+% subplot(2,1,1)
+% plot(m_1,t_1)
+% subplot(2,1,2)
+% plot(m_2,t_2)
 %% Conduction
 %Conduction from water, through glass, to air
 %The bottom is probably assumed to be 0.
